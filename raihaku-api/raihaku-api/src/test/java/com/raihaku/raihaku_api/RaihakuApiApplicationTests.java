7c1fb7bbb205882e367803c7c55a7a98f06cbd72
Brillyants <andrewbrillyant@gmail.com>
2025-01-30 08:06:18 +0700

    raihaku init commit

---
+++ raihaku-api/raihaku-api/src/test/java/com/raihaku/raihaku_api/RaihakuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raihaku.raihaku_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaihakuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
